520074a7c3fad1fb1a2457354238c87ffe0707ed
guanghaokwa <kwaguanghao@gmail.com>
2025-03-04 20:41:21 +0800

    Updated UI and added Project Section

--- js/main.js
+++ js/main.js
@@ -5,3 +5,38 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1000,
     loop: true
 });
+
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const overlay = document.querySelector('.overlay');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    menu.classList.toggle('active');
+    overlay.classList.toggle('active');
+
+    // Update aria-expanded for accessibility
+    const expanded = hamburger.getAttribute('aria-expanded') === 'true' || false;
+    hamburger.setAttribute('aria-expanded', !expanded);
+});
+
+// Close menu when clicking outside
+overlay.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    menu.classList.remove('active');
+    overlay.classList.remove('active');
+    hamburger.setAttribute('aria-expanded', 'false');
+});
+
+window.addEventListener('scroll', function () {
+    let scrollButton = document.getElementById('scrollTopButton');
+    if (window.scrollY > 200) {
+        scrollButton.style.display = 'block';
+    } else {
+        scrollButton.style.display = 'none';
+    }
+});
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
